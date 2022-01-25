c40079a3d22829823ac573292e8bf00d5e42ea8f
Fantasu <65580483+Fantasu@users.noreply.github.com>
2022-01-24 21:21:48 -0300

    refinamentos na hud

--- enemy_project/scripts/hazards/door.gd
+++ enemy_project/scripts/hazards/door.gd
@@ -4,7 +4,3 @@ extends Node2D
 func activate():
 	$AnimationPlayer.play("open")
 	GameEvents.emit_signal("call_shake", 0.2)
-	
-	
-func call_shake(trauma):
-	print(trauma)
--- enemy_project/scripts/ui/ui.gd
+++ enemy_project/scripts/ui/ui.gd
@@ -8,20 +8,30 @@ export (NodePath) onready var end = get_node(end)
 export (NodePath) onready var target = get_node(target)
 
 onready var map_distance = end.global_position.x - begin.global_position.x 
-onready var pos_distance = $End.global_position.x - $Begin.global_position.x
+onready var pos_distance = $Ui/End.global_position.x - $Ui/Begin.global_position.x
+
 
 func _ready():
+	GameEvents.connect("call_bars", self, "hide_or_show")
 	if target is Hero:
-		$Icon.texture = hero_sprite
+		$Ui/Icon.texture = hero_sprite
 #	elif target is Villain:
 #		$Icon.texture = villain_sprite
-	$Icon.global_position = $Begin.global_position
+	$Ui/Icon.global_position = $Ui/Begin.global_position
 
 
 func _process(delta):
 	var target_pos = target.global_position.x - begin.global_position.x 
 	var percent = target_pos / map_distance
-	$Icon.global_position.x = $Begin.global_position.x + percent * pos_distance
+	$Ui/Icon.global_position.x = $Ui/Begin.global_position.x + percent * pos_distance
 	
 	if target._direction:
-		$Icon.scale.x = target._direction
+		$Ui/Icon.scale.x = target._direction
+
+
+func hide_or_show(type):
+	match type:
+		0:
+			$Ui.visible = false
+		1:
+			$Ui.visible = true

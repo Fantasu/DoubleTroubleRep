396a6cd2c689f846b78e454fbe1b9e1136ef2023
Fantasu <65580483+Fantasu@users.noreply.github.com>
2022-01-28 23:53:00 -0300

    3 fase e ajustes

--- enemy_project/scripts/villain/villain.gd
+++ enemy_project/scripts/villain/villain.gd
@@ -29,7 +29,7 @@ func manage_animations():
 			animation_playback.travel('jump')
 			
 			
-	if Input.is_action_just_pressed("ui_down") && active == true && is_on_floor():
+	if Input.is_action_just_pressed("action") && active == true && is_on_floor():
 		animation_playback.travel("down_attack")
 
 
@@ -56,7 +56,7 @@ func destroy_tiles():
 	if is_instance_valid(_tile_map):
 		for child in $BreakPositions.get_children():
 			var tile_pos = _tile_map.world_to_map(child.global_position)
-			if _tile_map.get_cellv(tile_pos) == 1:
+			if _tile_map.get_cellv(tile_pos) == 5:
 				_tile_map.set_cellv(tile_pos, -1)
 				_tile_map.update_bitmask_area(tile_pos)
 					
@@ -73,24 +73,6 @@ func get_tile():
 		if collider is TileMap:
 			_tile_map = collider
 			$BreakTileRay.enabled = false
-
-
-func platform_state(delta):
-	if not _inside_wind:
-		_velocity.x = max(abs(_velocity.x) - _ground_fric, 0.0) * sign(_velocity.x)
-	
-	_velocity.x = clamp(_velocity.x, -ground_max_velocity, ground_max_velocity)
-	
-	if _direction != 0:
-#		$OneWayStatic/CollisionShape2D.disabled = true
-		_actual_state = STATE_MOVE
-	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		jump()
-	
-	if not is_on_floor():
-#		$OneWayStatic/CollisionShape2D.disabled = true
-		_actual_state = STATE_AIR
 
 
 func setting_active_property(new_value):
